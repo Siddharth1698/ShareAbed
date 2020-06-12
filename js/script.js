e868cb2dc8d267e2bc99643b2c04759648647057
Restyled.io <commits@restyled.io>
2020-06-12 23:05:57 +0000

    Restyled by prettier

--- js/script.js
+++ js/script.js
@@ -2,17 +2,14 @@ let btnAdd = document.querySelector("#add");
 let btnSubtract = document.querySelector("#subtract");
 let input = document.querySelector("input");
 
-btnAdd.addEventListener('click', () => {
+btnAdd.addEventListener("click", () => {
   input.value = parseInt(input.value) + 1;
 });
 
-btnSubtract.addEventListener('click', () => {
+btnSubtract.addEventListener("click", () => {
   if (input.value > 0) {
-
     input.value = parseInt(input.value) - 1;
-
   } else {
-
     input.value = parseInt(input.value) = 0;
   }
 });
