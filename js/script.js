0fae894eb28713a865e6859904b24a4652d6c2dd
rajat <rajatkhatri0002@gmail.com>
2020-06-14 00:52:44 +0530

    map added

--- js/script.js
+++ js/script.js
@@ -2,38 +2,54 @@ let btnAdd = document.querySelector("#add");
 let btnSubtract = document.querySelector("#subtract");
 let input = document.querySelector("input");
 
-btnAdd.addEventListener("click", () => {
-  input.value = parseInt(input.value) + 1;
+
+
+btnAdd.addEventListener('click',()=>{
+
+input.value=parseInt(input.value) +  1  ;
+
 });
 
-btnSubtract.addEventListener("click", () => {
-  if (input.value > 0) {
-    input.value = parseInt(input.value) - 1;
-  } else {
-    input.value = parseInt(input.value) = 0;
-  }
+btnSubtract.addEventListener('click',()=>{
+
+    if (input.value>0){
+    
+    input.value=parseInt(input.value) - 1;
+
+    }
+    else{
+
+        input.value=parseInt(input.value)=0;
+    }
+
+
+
 });
 
-function myFunction() {
-  var txt;
-  if (
-    confirm("Thanks for your donation.Medical authority will contact you soo")
-  ) {
-    txt = "Thankyou";
-  } else {
-    txt = "we appritiate that you are thinking to donate";
-  }
-  document.getElementById("addbed").innerHTML = txt;
+function myFunction(){
+    var txt;
+    if(confirm("Thanks for your donation.Medical authority will contact you soo")){
+
+        txt="Thankyou";
+
+    }
+    else{
+        txt="we appritiate that you are thinking to donate"
+    }
+    document.getElementById("addbed").innerHTML=txt;
+
 }
 
-function myFunction1() {
-  var txt;
-  if (
-    confirm("Thanks for your donation.Medical authority will contact you soo")
-  ) {
-    txt = "Thankyou";
-  } else {
-    txt = "we appritiate that you are thinking to donate";
-  }
-  document.getElementById("addroom").innerHTML = txt;
+function myFunction1(){
+    var txt;
+    if(confirm("Thanks for your donation.Medical authority will contact you soo")){
+
+        txt="Thankyou";
+
+    }
+    else{
+        txt="we appritiate that you are thinking to donate"
+    }
+    document.getElementById("addroom").innerHTML=txt;
+
 }
