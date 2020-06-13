aa300180794adf933b90d0841c0c94ad43b5dafe
Restyled.io <commits@restyled.io>
2020-06-13 19:26:17 +0000

    Restyled by clang-format

--- js/script.js
+++ js/script.js
@@ -2,54 +2,43 @@ let btnAdd = document.querySelector("#add");
 let btnSubtract = document.querySelector("#subtract");
 let input = document.querySelector("input");
 
-
-
-btnAdd.addEventListener('click',()=>{
-
-input.value=parseInt(input.value) +  1  ;
-
+btnAdd.addEventListener('click', () => {
+  input.value = parseInt(input.value) + 1;
 });
 
-btnSubtract.addEventListener('click',()=>{
+btnSubtract.addEventListener('click', () => {
+  if (input.value > 0) {
 
-    if (input.value>0){
-    
-    input.value=parseInt(input.value) - 1;
+    input.value = parseInt(input.value) - 1;
 
-    }
-    else{
+  } else {
 
-        input.value=parseInt(input.value)=0;
-    }
-
-
-
+    input.value = parseInt(input.value) = 0;
+  }
 });
 
-function myFunction(){
-    var txt;
-    if(confirm("Thanks for your donation.Medical authority will contact you soo")){
+function myFunction() {
+  var txt;
+  if (confirm(
+          "Thanks for your donation.Medical authority will contact you soo")) {
 
-        txt="Thankyou";
+    txt = "Thankyou";
 
-    }
-    else{
-        txt="we appritiate that you are thinking to donate"
-    }
-    document.getElementById("addbed").innerHTML=txt;
-
+  } else {
+    txt = "we appritiate that you are thinking to donate"
+  }
+  document.getElementById("addbed").innerHTML = txt;
 }
 
-function myFunction1(){
-    var txt;
-    if(confirm("Thanks for your donation.Medical authority will contact you soo")){
+function myFunction1() {
+  var txt;
+  if (confirm(
+          "Thanks for your donation.Medical authority will contact you soo")) {
 
-        txt="Thankyou";
+    txt = "Thankyou";
 
-    }
-    else{
-        txt="we appritiate that you are thinking to donate"
-    }
-    document.getElementById("addroom").innerHTML=txt;
-
+  } else {
+    txt = "we appritiate that you are thinking to donate"
+  }
+  document.getElementById("addroom").innerHTML = txt;
 }
