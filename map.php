30de1be507edd9f85f03bf420cd805b02f274069
Restyled.io <commits@restyled.io>
2020-06-13 19:26:25 +0000

    Restyled by whitespace

--- map.php
+++ map.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
-<html> 
-<head> 
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8" /> 
-  <title>Google Maps Multiple Markers</title> 
-  <script src="http://maps.google.com/maps/api/js?sensor=false" 
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <title>Google Maps Multiple Markers</title>
+  <script src="http://maps.google.com/maps/api/js?sensor=false"
           type="text/javascript"></script>
-</head> 
+</head>
 <body>
   <div id="map" style="width: 900px; height: 600px;"></div>
 
   <script type="text/javascript">
     var locations = [
-      
+
       ['DNG Orthopadic hospital', 26.850436,75.772991, 4],
       ['sharda hospital',26.845569,75.770825,6],
       ['raksheet hospital',26.848678,75.771488,5],
       ['mansarover hospital',26.847739,75.769823,7]
-     
-     
+
+
     ];
 
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -30,7 +30,7 @@
 
     var marker, i;
 
-    for (i = 0; i < locations.length; i++) {  
+    for (i = 0; i < locations.length; i++) {
       marker = new google.maps.Marker({
         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
         map: map
