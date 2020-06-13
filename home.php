911c0a2787d50649f0ddc707b815cbf02805d4c0
rajat <rajatkhatri0002@gmail.com>
2020-06-14 01:35:07 +0530

    payment merged

--- home.php
+++ home.php
@@ -61,7 +61,7 @@
 <!--/.Navbar -->
 <div class="d-flex justify-content-center bed">
    <div class="btn-group" role="group" >
-
+  
   <!-- Button trigger modal -->
 <button type="button" class="btn  btn-grey" data-toggle="modal" data-target="#basicExampleModal">
  Donate Bed
@@ -77,7 +77,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel" >Donate Beds</h5>
-        <div class="img">
+        <div class="img">      
             <img src="https://img.icons8.com/offices/30/000000/hospital-bed.png" />
         </div>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -85,9 +85,9 @@
         </button>
       </div>
       <div class="modal-body">
-        <p >Your donation will save someones life</p>
+        <p >Your donation will save someones life</p> 
         <p id="addbed" style="font-size: 20px; color: green;"></p>
-
+        
        </div>
 
 
@@ -113,12 +113,12 @@
           </div>
       </div>
 
-
+        
         <button type="button"  onclick="myFunction()" class="btn btn-primary">Make Donation</button>
+       
 
-
-
-
+ 
+        
 
       </div>
     </div>
@@ -133,7 +133,7 @@
       <div class="modal-header">
 
         <h5 class="modal-title" id="exampleModalLabel1">Donate a Room</h5>
-        <div class="img">
+        <div class="img">      
             <img src="img/ward.jpg"/>
         </div>
 
@@ -144,7 +144,7 @@
         </button>
       </div>
       <div class="modal-body">
-        <p> We Appritiate your Donation</p>
+        <p> We Appritiate your Donation</p> 
         <p id="addroom" style="font-size: 20px; color: green;"></p>
 
       </div>
@@ -179,8 +179,8 @@
 
 
 
-
-
+        
+          
 
         <button type="button"  onclick="myFunction1()"  class="btn btn-primary">Make Donation</button>
       </div>
@@ -206,7 +206,7 @@
                     <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
                       <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
                       <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
-
+                     
                     </div>
                   </nav>
                   <div class="tab-content py-6 px-6 px-sm-0" id="nav-tabContent">
@@ -216,14 +216,14 @@
                     <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
                       Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
                     </div>
-
-
+                  
+                 
                   </div>
-
+                
                 </div>
               </div>
         </div>
-
+    
 
 
 
@@ -242,8 +242,8 @@
   <script type="text/javascript" src="js/mdb.min.js"></script>
   <!-- Your custom scripts (optional) -->
   <script type="text/javascript"></script>
-  <script src="js/script.js"></script>
-  <script src="js/script2.js"></script>
+  <script src="js/script.js"></script>  
+  <script src="js/script2.js"></script>  
 
 
 </body>
