a1157a3e6929813a789bb88e33624b2135500210
Restyled.io <commits@restyled.io>
2020-06-13 15:48:44 +0000

    Restyled by prettier

--- js/script.js
+++ js/script.js
@@ -2,43 +2,38 @@ let btnAdd = document.querySelector("#add");
 let btnSubtract = document.querySelector("#subtract");
 let input = document.querySelector("input");
 
-btnAdd.addEventListener('click', () => {
+btnAdd.addEventListener("click", () => {
   input.value = parseInt(input.value) + 1;
 });
 
-btnSubtract.addEventListener('click', () => {
+btnSubtract.addEventListener("click", () => {
   if (input.value > 0) {
-
     input.value = parseInt(input.value) - 1;
-
   } else {
-
     input.value = parseInt(input.value) = 0;
   }
 });
 
 function myFunction() {
   var txt;
-  if (confirm(
-          "Thanks for your donation.Medical authority will contact you soo")) {
-
+  if (
+    confirm("Thanks for your donation.Medical authority will contact you soo")
+  ) {
     txt = "Thankyou";
-
   } else {
-    txt = "we appritiate that you are thinking to donate"
+    txt = "we appritiate that you are thinking to donate";
   }
   document.getElementById("addbed").innerHTML = txt;
 }
 
 function myFunction1() {
   var txt;
-  if (confirm(
-          "Thanks for your donation.Medical authority will contact you soo")) {
-
+  if (
+    confirm("Thanks for your donation.Medical authority will contact you soo")
+  ) {
     txt = "Thankyou";
-
   } else {
-    txt = "we appritiate that you are thinking to donate"
+    txt = "we appritiate that you are thinking to donate";
   }
   document.getElementById("addroom").innerHTML = txt;
 }
--- js/script2.js
+++ js/script2.js
@@ -2,17 +2,14 @@ let btnadd = document.querySelector("#sum");
 let btnsubtract = document.querySelector("#minus");
 let inp = document.querySelector("input");
 
-btnadd.addEventListener('click', () => {
+btnadd.addEventListener("click", () => {
   inp.value = parseInt(inp.value) + 1;
 });
 
-btnsubtract.addEventListener('click', () => {
+btnsubtract.addEventListener("click", () => {
   if (inp.value > 0) {
-
     inp.value = parseInt(inp.value) - 1;
-
   } else {
-
     inp.value = parseInt(inp.value) = 0;
   }
 });
